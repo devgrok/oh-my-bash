f8f94db4cb655b9bb0ce4caaeb33632f88e1aed0
Chris Watts <devgrok@users.noreply.github.com>
2020-10-11 20:19:22 +1100

    aws asp completions

--- plugins/aws/aws.plugin.sh
+++ plugins/aws/aws.plugin.sh
@@ -14,7 +14,28 @@ function agp {
 
 function asp {
   local rprompt=${RPROMPT/<aws:$(agp)>/}
+  if [[ -z "$1" ]]; then
+    unset AWS_DEFAULT_PROFILE AWS_PROFILE AWS_EB_PROFILE
+    echo AWS profile cleared.
+    return
+  fi  
 
   export AWS_DEFAULT_PROFILE=$1
   export AWS_PROFILE=$1
 }
+
+function aws_profiles() {
+  [[ -r "${AWS_CONFIG_FILE:-$HOME/.aws/config}" ]] || return 1
+  grep '\[profile' "${AWS_CONFIG_FILE:-$HOME/.aws/config}"|sed -e 's/.*profile \([a-zA-Z0-9@_\.-]*\).*/\1/'
+}
+
+function _aws_profiles() {  
+  local cur prev opts;
+  _init_completion -n : || return;
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  #_init_completion -n : || return;
+  COMPREPLY=( $(compgen -W "$(aws_profiles)" -- ${cur} ) )
+}
+complete -F _aws_profiles asp
+
